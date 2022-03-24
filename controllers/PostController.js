ddfc7263731ded8e5a18217bc6db39ba5de462d2
raquelMoya <rakelms90@gmail.com>
2022-03-24 09:55:20 +0100

    Initializing createPost

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,10 +1,22 @@
 const Post = require("../models/Post.js");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const authConfig = require("../config/auth");
 const jwt = require("jsonwebtoken");
 
 const PostController = {};
 
 PostController.create = async (req, res) => {
+   
+        const post = req.body;
+    
+        const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
+    
+        try {
+            await newPost.save();
+    
+            res.status(201).json(newPost);
+        } catch (error) {
+            res.status(409).json({ message: error.message });
+        }
     
 };
