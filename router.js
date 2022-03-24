64c65da86314edb92ff90695e95137901dbb0d59
raquelMoya <rakelms90@gmail.com>
2022-03-24 10:18:22 +0100

    created routes of posts

--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const users = require('./routes/users');
+const posts = require('./routes/posts');
 
 router.use('/users', users);
+router.use('posts', posts);
 
 module.exports = router;
