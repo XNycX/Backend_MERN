46526738ebb4ee22e0b9a60e4979416cc14d8cf1
raquelMoya <rakelms90@gmail.com>
2022-03-24 09:40:55 +0100

    post models created

---
+++ controllers/PostController.js
@@ -0,0 +1,10 @@
+const Post = require("../models/Post.js");
+const bcrypt = require("bcrypt");
+const authConfig = require("../config/auth");
+const jwt = require("jsonwebtoken");
+
+const PostController = {};
+
+PostController.create = async (req, res) => {
+    
+};
