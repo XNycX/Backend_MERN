bc668cee69742af76cbbf51d3aa03018eb4d3302
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-03-26 11:36:58 +0100

    changes added

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,14 @@ const { authentication, isAdmin } = require('../middleware/authentication');
 const UserController = require('../controllers/UserController');
 
 // http://localhost:5500/users/register (usando un POST).
-router.post('/register', UserController.register);
 router.get('/confirm/:emailToken', UserController.confirmEmail);
-router.delete('/delete', UserController.deleteById);
 router.get('/', UserController.getAllUsers);
 router.get('/nickname', UserController.getUserByNickname);
+router.post('/register', UserController.register);
 router.post('/followers', UserController.following);
 router.post('/unfollow', UserController.unfollowing);
 router.post('/login', UserController.login);
+router.put('/edit/:id', UserController.updateUser);
+router.delete('/delete', UserController.deleteById);
 
 module.exports = router;
