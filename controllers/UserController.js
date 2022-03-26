57d592c99a2faf28fbfdb34830a48351b4571a78
XNycX <nycx2009@hotmail.com>
2022-03-26 12:19:59 +0100

    edit user created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,7 +25,6 @@ UserController.register = async (req, res) => {
     const emailToken = jwt.sign({ email: req.body.email }, authConfig.secret, {
       expiresIn: authConfig.expires,
     });
-    console.log(emailToken);
     const url = "http://localhost:5500/users/confirm/" + emailToken;
     await transporter.sendMail({
       to: req.body.email,
@@ -83,130 +82,148 @@ UserController.getUserByNickname = async (req, res) => {
 };
 //Para seguir a otro usuario
 UserController.following = async (req, res) => {
+  let _id = req.body._id;
 
-  let _id = req.body._id
-
-  let id_userToFollow = req.body.following
+  let id_userToFollow = req.body.following;
 
   // Enviar Mensaje al usuario que ya sigue a esa persona
   try {
-      await User.findOneAndUpdate(
-          { _id: _id },
-          {
-              $push: {
-                  following: {
-                      "following": id_userToFollow
-                  }
-              }
-          }
-      )
-      res.send("Has Comenzado a seguir a esta persona")
-
+    await User.findOneAndUpdate(
+      { _id: _id },
+      {
+        $push: {
+          following: {
+            following: id_userToFollow,
+          },
+        },
+      }
+    );
+    res.send("Has Comenzado a seguir a esta persona");
   } catch (error) {
-      res.send(error)
+    res.send(error);
   }
 };
 //Para añadir un seguidor a un usuario
 UserController.followers = async (req, res) => {
+  let _id = req.body._id;
 
-  let _id = req.body._id
-
-  let id_follower = req.body.follower
+  let id_follower = req.body.follower;
 
   // Enviar Mensaje al usuario que ya sigue a esa persona
   try {
-      await User.findOneAndUpdate(
-          { _id: _id },
-          {
-              $push: {
-                  followers: {
-                      "followers": id_follower
-                  }
-              }
-          }
-      )
-      res.send(`Te sigue ${id_follower}`)
-
+    await User.findOneAndUpdate(
+      { _id: _id },
+      {
+        $push: {
+          followers: {
+            followers: id_follower,
+          },
+        },
+      }
+    );
+    res.send(`Te sigue ${id_follower}`);
   } catch (error) {
-      res.send(error)
+    res.send(error);
   }
 };
 //Para quitar un seguidor a un usuario
 UserController.unfollowed = async (req, res) => {
+  let _id = req.body._id;
 
-  let _id = req.body._id
-
-  let id_follower = req.body.follower
+  let id_follower = req.body.follower;
 
   try {
-      await User.findOneAndUpdate(
-          { _id: _id },
-          {
-              $pull: {
-                  followers: {
-                      "followed": id_follower
-                  }
-              }
-          }
-      )
-      res.send(`Te ha dejado de seguir ${id_follower}.`)
-
+    await User.findOneAndUpdate(
+      { _id: _id },
+      {
+        $pull: {
+          followers: {
+            followed: id_follower,
+          },
+        },
+      }
+    );
+    res.send(`Te ha dejado de seguir ${id_follower}.`);
   } catch (error) {
-      res.send(error)
+    res.send(error);
   }
 };
 //Para dejar de seguir a un usuario
 UserController.unfollowing = async (req, res) => {
+  let _id = req.body._id;
 
-  let _id = req.body._id
-
-  let id_userToUnfollow = req.body.unfollowed
+  let id_userToUnfollow = req.body.unfollowed;
 
   try {
-      await User.findOneAndUpdate(
-          { _id: _id },
-          {
-              $pull: {
-                  following: {
-                      "following": id_userToUnfollow
-                  }
-              }
-          }
-      )
-      res.send(`Has dejado de seguir a ${id_userToUnfollow}.`)
-
+    await User.findOneAndUpdate(
+      { _id: _id },
+      {
+        $pull: {
+          following: {
+            following: id_userToUnfollow,
+          },
+        },
+      }
+    );
+    res.send(`Has dejado de seguir a ${id_userToUnfollow}.`);
   } catch (error) {
-      res.send(error)
+    res.send(error);
   }
 };
 UserController.login = (req, res) => {
-    let password = req.body.password;
-    let email = req.body.email;
-    User.findOne({email: req.body.email}).then((user) => {
-      if (!user) {
-        return res
-          .status(400)
-          .send({ message: "Usuario o contraseña incorrectos" });
-      }
-      const isMatch = bcrypt.compareSync(password, user.password);
-      if (!isMatch) {
-        return res
-          .status(400)
-          .send({ message: "Usuario o contraseña incorrectos" });
-      }
-      if (!user.confirmed) {
-        return res.status(400).send({
-          message:
-            "Debes confirmar tu correo, recuerda revisar tu carpeta de SPAM si no ves nuestro correo de confirmación",
-        });
-      }
-
-      token = jwt.sign({ _id: user._id }, authConfig.secret, {
-        expiresIn: authConfig.expires,
+  let password = req.body.password;
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (!user) {
+      return res
+        .status(400)
+        .send({ message: "Usuario o contraseña incorrectos" });
+    }
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .send({ message: "Usuario o contraseña incorrectos" });
+    }
+    if (!user.confirmed) {
+      return res.status(400).send({
+        message:
+          "Debes confirmar tu correo, recuerda revisar tu carpeta de SPAM si no ves nuestro correo de confirmación",
       });
-      res.send({ message: `Bienvenid@ ${user.nickname}, tu token es: ${token}` });
-    });
-};
+    }
 
+    token = jwt.sign({ _id: user._id }, authConfig.secret, {
+      expiresIn: authConfig.expires,
+    });
+    res.send({ message: `Bienvenid@ ${user.nickname}`,token });
+  });
+};
+UserController.updateUser = async (req, res) => {
+  let id = req.user._id;
+  try {
+    const users = await User.find();
+    const nicknameExist = users.some(user => user.nickname == req.body.nickname)
+    if (nicknameExist) {
+      res.send('Este nickname ya existe')
+    } else {
+      const userUpdated= await User.findByIdAndUpdate(id, {
+         $set: {
+           nickname: req.body.nickname,
+           city: req.body.city,
+           image_path: req.body.image_path,
+         },
+       },
+       { new: true })
+       res.status(200).json({ msg: "Has modificado tu usuario",userUpdated });
+    }
+
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        msg: `Tu mensaje`,
+        error: { name: error.name, message: error.message, detail: error },
+      });
+  }
+};
 
 module.exports = UserController;
