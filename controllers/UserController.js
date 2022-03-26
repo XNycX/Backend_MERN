7e9f566da6df4b33be36e22dd0897fc9a2f57499
XNycX <nycx2009@hotmail.com>
2022-03-26 17:08:13 +0100

    wrong name on Usercontroller for require models user.js fixed

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User.js");
+const User = require("../models/user.js");
 const bcrypt = require("bcryptjs");
 const authConfig = require("../config/auth");
 const jwt = require("jsonwebtoken");
