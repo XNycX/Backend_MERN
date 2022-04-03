e82a51f7b48a1c27f3b4226cf344d3b323578bbb
XNycX <nycx2009@hotmail.com>
2022-04-03 19:23:34 +0200

    fix: middleware authory fixed

--- middleware/isAuthor.js
+++ middleware/isAuthor.js
@@ -4,8 +4,7 @@ const Comment = require("../models/comments.js");
 const isAuthor_Post = async (req, res, next) => {
   let idPost = req.params._id;
   try {
-    const post = await Post.findById(idPost);
-    if (post.creatorId.toString() !== req.user._id.toString()) {
+    if (idPost.toString() !== req.user._id.toString()) {
       return res.status(403).send({ message: "No eres el creador de este post" });
     }
     
